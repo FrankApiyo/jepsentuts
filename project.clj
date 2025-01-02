b88ac4e11f2fad60ddb4d840663352b4cdcbbcc4
FrankApiyo <franklineapiyo@gmail.com>
2025-01-02 15:41:34 +0300

    Add a noop etcd test

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject jepsen.etcdemo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A jepsen test for etcd"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main jepsen.etcdemo
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [jepsen "0.2.1-SNAPSHOT"]
+                 [verschlimmbesserung "0.1.3"]]
   :repl-options {:init-ns jepsen.etcdemo})
--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -1,6 +1,23 @@
-(ns jepsen.etcdemo)
+(ns jepsen.etcdemo
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), construct a new test map."
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true}
+         (merge opts
+                {:ssh {:username "root"
+                       :password "root"
+                       :dummy? true
+                       :strict-host-key-checking false
+                       :private-key-path nil}})))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  [& args]
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test}) (cli/serve-cmd))
+            args))
